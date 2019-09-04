fea5c0b87ba0b6d74d454c61ebb40d31b7259ccb
Julien Lengrand-Lambert <julien@lengrand.fr>
2019-09-04 11:32:10 +0200

    Created our memory grid

--- src/Main.elm
+++ src/Main.elm
@@ -19,31 +19,6 @@ init =
     ( {}, Cmd.none )
 
 
-numberColumns : Int
-numberColumns =
-    4
-
-
-numberRows : Int
-numberRows =
-    4
-
-
-numberOfImages : Int
-numberOfImages =
-    13
-
-
-takeImagesFromCatalog : Int -> Int -> List Int
-takeImagesFromCatalog sampleSize totalSize =
-    List.range 1 totalSize 
-
-
-intListToString : List Int -> String
-intListToString myList =
-    String.concat <| List.map (\s -> String.fromInt s ++ ",") myList
-
-
 
 ---- UPDATE ----
 
@@ -63,32 +38,24 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    Element.layout []
-        (Element.column
-            [ Element.spacing 40 ]
-            [ Element.image
-                [ Element.width <| Element.px 300
-                , Element.height <| Element.px 300
-                , Element.padding 20
-                ]
-                { src = "logo.svg", description = "Elm logo" }
-            , Element.text "Your Elm App is working!"
-            , Element.text <| intListToString <| takeImagesFromCatalog 3 13
-            , el []
-                (Element.column []
-                    (List.range 1 numberColumns
-                        |> List.map
-                            (\col ->
-                                List.range 1 numberRows
-                                    |> List.map
-                                        (\row -> Element.text (String.fromInt row ++ String.fromInt col))
-                            )
-                        |> List.concat
+    Element.layout [] (
+        Element.column [] [
+            Element.image [Element.width <| Element.px 300, Element.height <| Element.px 300] {
+                src= "logo.svg",
+                description= "The Elm Logo"
+            }
+            , Element.text "Our memory game" 
+            , Element.row [] 
+                (List.range 1 4
+                    |> List.map (\columnNumber ->
+                        Element.column [] ( 
+                        List.range 1 4 
+                            |> List.map (\rowNumber -> Element.text <| "[" ++ String.fromInt rowNumber ++ String.fromInt columnNumber ++ "]")
+                        )
                     )
                 )
-            ]
-        )
-
+        ]
+    )
 
 
 ---- PROGRAM ----
